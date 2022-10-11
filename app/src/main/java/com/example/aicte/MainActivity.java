7b656dcaf9fac16aa7edf167d0802d110eee7e94
yashmahajan29 <yashsmahajan82@gmail.com>
2022-10-11 23:35:39 +0530

    added reg auth firebase

--- app/src/main/java/com/example/aicte/MainActivity.java
+++ app/src/main/java/com/example/aicte/MainActivity.java
@@ -1,14 +1,83 @@
 package com.example.aicte;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEmail , mPass;
+    private TextView mTextView;
+    private Button signupBtn;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEmail = findViewById(R.id.email_reg);
+        mPass = findViewById(R.id.passreg);
+        mTextView = findViewById(R.id.textView);
+        signupBtn = findViewById(R.id.registration_btn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUser();
+            }
+        });
+
+    }
+    private void createUser() {
+        String email = mEmail.getText().toString();
+        String pass = mPass.getText().toString();
+
+        if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            if (!pass.isEmpty()){
+
+                mAuth.createUserWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                Toast.makeText(MainActivity.this, "Registered Successfully !!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(MainActivity.this , Sign_in_activity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "Registration Error", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }else{
+                mPass.setError("Empty fields are not allowed");
+            }
+        }else if(email.isEmpty()){
+            mEmail.setError("Empty fields are not allowed");
+        }else{
+            mEmail.setError("Please Enter Correct Email");
+        }
     }
 }
